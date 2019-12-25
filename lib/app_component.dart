c5fd9f1d67b699b4a7b023bef7938f72d041f5f1
hiyoki <maha.tree.21@gmail.com>
2019-12-26 07:06:18 +0900

    routes , before Router links

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,15 +3,16 @@ import 'package:angular_router/angular_router.dart';
 
 import 'src/hero_service.dart';
 import 'src/hero_list_component.dart';
+import 'src/routes.dart';
 
 @Component(
   selector: 'my-app',
   template: '''
-    <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+   <router-outlet [routes]="Routes.all"></router-outlet>
   ''',
-  directives: [HeroListComponent],
+  directives: [routerDirectives],
   providers: [ClassProvider(HeroService)],
+  exports: [RoutePaths,Routes],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
