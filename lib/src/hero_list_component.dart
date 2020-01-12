f085de2ca9946f49cd4cee3d5e09e69332d0b308
hiyoki <maha.tree.21@gmail.com>
2020-01-13 08:45:28 +0900

    before style the app

--- lib/src/hero_list_component.dart
+++ lib/src/hero_list_component.dart
@@ -1,26 +1,30 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'dart:async';
 
 import 'hero.dart';
 import 'hero_component.dart';
 import 'mock_heroes.dart';
 import 'hero_service.dart';
+import 'route_paths.dart';
 
 
 @Component(
   selector: 'my-heroes',
   templateUrl: 'hero_list_component.html',
-  directives: [coreDirectives, HeroComponent],
+  directives: [coreDirectives],
   styleUrls: ['hero_list_component.css'],
+  pipes: [commonPipes],
 //  providers: [ClassProvider(HeroService)],
 )
 class HeroListComponent implements OnInit {
 
   final HeroService _heroService;
+  final Router _router;
   List<Hero> heroes;
   Hero selected;
 
-  HeroListComponent(this._heroService);
+  HeroListComponent(this._heroService, this._router);
 
   Future<void> _getHeroes() async {
     heroes = await _heroService.getAll();
@@ -29,6 +33,12 @@ class HeroListComponent implements OnInit {
   void ngOnInit() => _getHeroes();
 
   void onSelect(Hero hero) => selected = hero;
+
+  String _heroUrl(int id) =>
+      RoutePaths.hero.toUrl(parameters: {idParam: '$id'});
+  
+  Future<NavigationResult> gotoDetail() =>
+      _router.navigate(_heroUrl(selected.id));
 }
 
 
